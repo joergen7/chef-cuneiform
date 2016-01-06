cb5018bd9b3e9ecd3b9f5bd91d5c36e987fefae8
Jorgen Brandt <joergen.brandt@onlinehome.de>
2016-01-06 19:48:28 +0100

    Uberjar build incorporated.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,14 +5,13 @@
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
 cuneiform_dir = "#{node.dir.software}/cuneiform"
-cuneiform_bin = "#{cuneiform_dir}/cuneiform-dist/target/cuneiform-dist-2.0.2-SNAPSHOT/cuneiform-2.0.2-SNAPSHOT"
+cuneiform_bin = "#{cuneiform_dir}/cuneiform-dist/target"
 
 package "openjdk-7-jdk"
 package "r-base"
 package "maven"
 package "scala"
 package "octave"
-package "bsh"
 package "clisp"
 package "git"
 package "graphviz"
@@ -23,6 +22,7 @@ git "git_clone_cuneiform" do
   action :checkout
   repository node.github.cuneiform
   destination cuneiform_dir
+  revision "master"
 end
 
 bash "mvn_compile_cuneiform" do
@@ -36,7 +36,7 @@ end
 file "#{node.dir.bin}/logview" do
   content <<-CONTENT
 #!/usr/bin/env bash
-java -cp \"#{cuneiform_bin}:#{cuneiform_bin}/lib/*:#{cuneiform_bin}/bin/*\" de.huberlin.wbi.cuneiform.logview.main.Main $@  
+java -cp #{cuneiform_bin}/cuneiform-dist-2.0.3-SNAPSHOT.jar de.huberlin.wbi.cuneiform.logview.main.Main $@  
   CONTENT
   mode '0755'
 end
@@ -44,7 +44,7 @@ end
 file "#{node.dir.bin}/cfide" do
   content <<-CONTENT
 #!/usr/bin/env bash
-java -cp \"#{cuneiform_bin}:#{cuneiform_bin}/lib/*:#{cuneiform_bin}/bin/*\" de.huberlin.wbi.cuneiform.cfide.main.Main $@  
+java -cp #{cuneiform_bin}/cuneiform-dist-2.0.3-SNAPSHOT.jar de.huberlin.wbi.cuneiform.cfide.main.Main $@  
   CONTENT
   mode '0755'
 end
@@ -52,7 +52,7 @@ end
 file "#{node.dir.bin}/cuneiform" do
   content <<-CONTENT
 #!/usr/bin/env bash
-nice -n 19 java -cp \"#{cuneiform_bin}:#{cuneiform_bin}/lib/*:#{cuneiform_bin}/bin/*\" de.huberlin.wbi.cuneiform.cmdline.main.Main -l /tmp/cf-cache $@  
+nice -n 19 java -jar #{cuneiform_bin}/cuneiform-dist-2.0.3-SNAPSHOT.jar -l /tmp/cf-cache $@  
   CONTENT
   mode '0755'
 end
