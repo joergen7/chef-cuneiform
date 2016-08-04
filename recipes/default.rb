11a694f835acda693f13e337b13ec40e45613379
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-08-04 10:00:12 +0200

    Cookbook cleaned up.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,20 +4,21 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-cuneiform_dir = "#{node.dir.software}/cuneiform"
-cuneiform_bin = "#{cuneiform_dir}/_build/default/bin"
-cuneiform_vsn = "2.2.0-release"
+cuneiform_vsn    = "2.2.0-release"
+cuneiform_dir    = "#{node["dir"]["software"]}/cuneiform-#{cuneiform_vsn}"
+cuneiform_bin    = "#{cuneiform_dir}/_build/default/bin"
+cuneiform_github = "https://github.com/joergen7/cuneiform.git"
 
 include_recipe "chef-misc::rebar3"
 
 package "r-base"
 package "rlwrap"
 
-directory node.dir.software
+directory node["dir"]["software"]
 
 git "git_clone_cuneiform" do
   action :checkout
-  repository node.github.cuneiform
+  repository cuneiform_github
   destination cuneiform_dir
   revision cuneiform_vsn
 end
@@ -25,10 +26,10 @@ end
 bash "install_cuneiform" do
   code "make"
   cwd cuneiform_dir
-  not_if "#{File.exists?( "#{cuneiform_dir}/_build/default/bin/cuneiform" )}"
+  creates "#{cuneiform_dir}/_build/default/bin/cuneiform"
 end
 
-file "/usr/local/bin/cuneiform" do
+file "#{node["dir"]["bin"]}/cuneiform" do
   content "rlwrap #{cuneiform_dir}/_build/default/bin/cuneiform $@"
   mode "0755"
 end
