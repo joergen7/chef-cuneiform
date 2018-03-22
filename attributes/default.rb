f1efa0660f83535515c72b87c0f0c010acb7625b
Jörgen <joergen.brandt@onlinehome.de>
2018-03-22 03:33:42 +0100

    Increase version to 0.1.3 and upgrade cuneiform to 3.0.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default["dir"]["software"]     = "/opt/software"
 default["dir"]["bin"]          = "/usr/local/bin"
 
-default["cuneiform"]["vsn"]    = "3.0.1"
+default["cuneiform"]["vsn"]    = "3.0.2"
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joergen.brandt@onlinehome.de'
 license          'Apache 2.0'
 description      'Installs Cuneiform'
 long_description 'Installs Cuneiform'
-version          '0.1.2'
+version          '0.1.3'
 
 depends "chef-misc", "0.1.1"
